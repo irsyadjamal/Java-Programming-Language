22ab0dfdfb75c5c29712aa5732321867c611070e
irsyadjamal <irsyadjamal@users.noreply.github.com>
2014-03-10 21:26:54 +0700

    Update Selection01_03.java

--- Selection01_03.java
+++ Selection01_03.java
@@ -1,5 +1,18 @@
 import java.util.Scanner;
 
 public class Selection01_03{
-
+  public static void main (String[]args){
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.print("Enter an integer: ");
+    int integer = scan.nextInt();
+    
+    if (integer % 5 == 0) {
+      System.out.println("HiFive");
+    }
+    
+    if (integer % 2 == 0) {
+      System.out.println("HiEven");
+    }
+  }
 }
